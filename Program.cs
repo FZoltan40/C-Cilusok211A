e6ee5be9a7017dbe0c079d617d7cc0334523166e
FZoltan40 <farkasz@kkszki.hu>
2025-09-25 09:46:26 +0200

    Feladat9 is ready.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_Ciklusokfeladat2
 {
@@ -10,6 +7,34 @@ namespace C_Ciklusokfeladat2
     {
         static void Main(string[] args)
         {
+            List<int> szamok = new List<int>();
+
+            int db = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        db++;
+                    }
+                }
+
+                if (db <= 2)
+                {
+                    szamok.Add(i);
+                }
+
+                db = 0;
+            }
+
+            foreach (var item in szamok)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine($"{szamok.Count} prím szám van.");
         }
     }
 }
