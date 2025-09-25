a50b3b3f2eebd16a80c467d80f5f9786979d31b6
FZoltan40 <farkasz@kkszki.hu>
2025-09-25 10:36:05 +0200

    Feladat10 is ready.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace C_Ciklusokfeladat2
 {
@@ -7,7 +8,48 @@ namespace C_Ciklusokfeladat2
     {
         static void Main(string[] args)
         {
-            List<int> szamok = new List<int>();
+            Dictionary<int, string> dobasEredmeny = new Dictionary<int, string>();
+            dobasEredmeny.Add(1, "Kő");
+            dobasEredmeny.Add(2, "Papír");
+            dobasEredmeny.Add(3, "Olló");
+
+            int gepTalalat = 0;
+            int talalat = 0;
+
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Kérem a tipped:");
+                string tipp = Console.ReadLine().ToLower();
+
+                string gepTipp = dobasEredmeny.Values.ElementAt(rnd.Next(3)).ToLower();
+                Console.WriteLine(gepTipp);
+
+                if (tipp == gepTipp)
+                {
+                    Console.WriteLine("ugyanaz");
+                }
+                else if (tipp == "olló" && gepTipp == "papír" || tipp == "kő" && gepTipp == "papír" ||
+                    tipp == "papír" && gepTipp == "kő")
+                {
+                    Console.WriteLine("Te nyertél");
+                    talalat++;
+                }
+                else
+                {
+                    Console.WriteLine("Gép nyert");
+                    gepTalalat++;
+                }
+            }
+
+            if (talalat > gepTalalat)
+                Console.WriteLine("A játkot te nyerted.");
+            else
+                Console.WriteLine("A játékot a gép nyerte.");
+
+
+
+            /*List<int> szamok = new List<int>();
 
             int db = 0;
 
@@ -34,7 +76,7 @@ namespace C_Ciklusokfeladat2
                 Console.WriteLine(item);
 
             }
-            Console.WriteLine($"{szamok.Count} prím szám van.");
+            Console.WriteLine($"{szamok.Count} prím szám van.");*/
         }
     }
 }
